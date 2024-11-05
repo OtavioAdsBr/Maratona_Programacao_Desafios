062c88effa4397226568e3c0591bbbf6ef61cd33
Otavio Vinicius Rosa <100728902+OtavioAdsBr@users.noreply.github.com>
2024-11-05 17:23:45 -0300

    Add files via upload

---
+++ Desafios da Maratona/Área do circulo.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+double raio, area;
+const double PI = 3.14159;
+
+cin >> raio;
+area = PI * (raio * raio);
+
+cout << fixed << setprecision(4);
+cout << "A=" << area << endl;
+
+    return 0;
+}
