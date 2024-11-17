6c2e8f177fa6dfc6b3daea95cefbc69c041562d9
Otavio Vinicius Rosa <100728902+OtavioAdsBr@users.noreply.github.com>
2024-11-17 20:31:54 -0300

    Add files via upload

---
+++ Desafios da Maratona/Média #2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+double A,B,C;
+
+cin >> A >> B >> C;
+double MEDIA = (A * 2 + B * 3 + C * 5) / 10;
+cout << fixed << setprecision(1);
+cout << "MEDIA = " << MEDIA << endl;
+
+    return 0;
+}
---
+++ Desafios da Maratona/Produto simples.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+double A, B, PROD;
+
+cin >> A >> B;
+PROD = A * B;
+cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
